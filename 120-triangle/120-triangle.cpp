2e603d3b9267266152bb023cd76eba0c11c40e3f
Heer <69032140+blue-rock@users.noreply.github.com>
2022-05-26 15:34:26 +0530

    Time: 6 ms (78.09%), Space: 8.7 MB (68.63%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int rows = triangle.size();
+        int dp[rows+1];
+        memset(dp,0,sizeof(dp));//all 0's
+        for(int i=rows-1;i>-1;i--)
+        {
+            for(int j=0;j<triangle[i].size();j++)
+            {
+                dp[j] = triangle[i][j] + min(dp[j],dp[j+1]);
+            }
+        }
+        return dp[0];
+    }
+};
