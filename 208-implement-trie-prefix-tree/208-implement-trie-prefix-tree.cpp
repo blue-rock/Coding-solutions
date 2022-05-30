d8118a6856592f31ea05cea4b2a1cd712d7b4b02
Heer <69032140+blue-rock@users.noreply.github.com>
2022-05-30 17:36:08 +0530

    Time: 75 ms (68.89%), Space: 48.4 MB (16.11%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,65 @@
+class TrieNode
+{
+  public:
+    vector<TrieNode*>child;
+    bool isEnd;
+    TrieNode(){
+        isEnd = false;
+        child.resize(26,nullptr);
+    }
+};
+class Trie {
+public:
+
+        TrieNode* root;
+        Trie()
+        {
+            root = new TrieNode();
+        }
+    void insert(string s) {
+        TrieNode* node = root;
+        for(int i=0;i<s.length();i++)
+        {
+            if(node->child[s[i]-'a']==nullptr)
+            {
+                node->child[s[i]-'a'] = new TrieNode();
+            }
+            node = node->child[s[i]-'a'];
+        }
+        node->isEnd = true;
+    }
+    
+    bool search(string s) {
+        TrieNode* node = root;
+        for(int i=0;i<s.length();i++)
+        {
+            if(node->child[s[i]-'a']==nullptr)
+            {
+                return false;
+            }
+            node = node->child[s[i]-'a'];
+        }
+        return node->isEnd;
+    }
+    
+    bool startsWith(string s) {
+        TrieNode* node = root;
+        for(int i=0;i<s.length();i++)
+        {
+            if(node->child[s[i]-'a']==nullptr)
+            {
+                return false;
+            }
+            node = node->child[s[i]-'a'];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
