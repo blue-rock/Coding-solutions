09ade94c69ee93a225569eabec031d960eb74b21
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-01 00:01:57 +0530

    Time: 88 ms (72.86%), Space: 35.5 MB (65.71%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    int countPairs(TreeNode* root, int distance) {
+        helper(root,distance);
+        return count;
+    }
+    vector<int> helper(TreeNode* root, int d)
+    {
+        if(root==NULL)
+        {
+            return {0};
+        }
+        if(root->right==NULL && root->left==NULL)
+        {
+            return {1};
+        }
+        vector<int>left = helper(root->left,d);
+        vector<int>right = helper(root->right,d);
+        for(auto x: left)
+        {
+            for(auto y:right)
+            {
+                if(x>0 && y>0)
+                {
+                    if(x+y<=d)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        vector<int>ans;
+        for(auto x:left)
+        {
+            if(x>0 && x<d)
+            {
+                ans.push_back(x+1);
+            }
+        }
+        for(auto x:right)
+        {
+            if(x>0 && x<d)
+            {
+                ans.push_back(x+1);
+            }
+        }
+        return ans;
+    }
+};
+//https://www.youtube.com/watch?v=4T2EjBpalpM
