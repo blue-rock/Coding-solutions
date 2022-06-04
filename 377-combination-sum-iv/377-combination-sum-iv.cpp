34a33f07bfea29eb348f89a2945c4493751967f5
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-05 01:17:25 +0530

    Time: 5 ms (31.61%), Space: 6.5 MB (99.41%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[1001];
+    int combinationSum4(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
+        dp[0] = 1;
+        return helper(nums,target);
+    }
+    int helper(vector<int>& nums,int target)
+    {
+        if(dp[target]>-1)
+        {
+            return dp[target];
+        }
+        int res = 0;
+        for(auto x:nums)
+        {
+            if(x<=target)
+            {
+                res = res + helper(nums,target-x);
+            }
+        }
+        dp[target] = res;
+        return dp[target];
+    }
+};
