dea67b0b02f55964fadc0aa3abcc736a30f8f5fc
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-06 20:26:11 +0530

    Time: 706 ms (12.66%), Space: 17.1 MB (35.91%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0 || n==1)
+        {
+            return 0;
+        }
+        vector<int>dp(n,100000);
+        dp[0] = 0;
+        for(int i=0;i<n-1;i++)
+        {
+            int dst = i + nums[i];
+                for(int j=i+1;j<=min(dst,n-1);j++)
+                {
+                    dp[j] = min(dp[j],dp[i] + 1);
+                }
+        }
+        return dp[n-1];
+    }
+};
