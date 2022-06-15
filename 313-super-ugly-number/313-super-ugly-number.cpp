f08899aaee60e3b6eedf59db65a963ab6091bd08
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-15 13:37:23 +0530

    Time: 87 ms (90.60%), Space: 8.9 MB (84.40%) - LeetHub

---
+++ 313-super-ugly-number/313-super-ugly-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int m = primes.size();
+        vector<int>pointer(m,0);
+        vector<int>dp(n,0);
+        dp[0] = 1;
+        for(int i=1;i<n;i++)
+        {
+            int mi = INT_MAX;
+            for(int j=0;j<m;j++)
+            {
+                mi = min(mi,primes[j]*dp[pointer[j]]);
+            }
+            dp[i] = mi;
+            for(int j=0;j<m;j++)
+            {
+                if(dp[i]==primes[j]*dp[pointer[j]])
+                {
+                    pointer[j]++;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
