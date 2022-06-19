681af19677e1867e7a39ba54bff8c453e77f75d8
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-19 08:58:42 +0530

    Time: 43 ms (96.69%), Space: 26.4 MB (85.68%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        vector<vector<string>>res;
+        sort(products.begin(),products.end());
+        int l = 0;
+        int r = products.size()-1;
+        for(int i=0;i<searchWord.length();i++)
+        {
+            auto c = searchWord[i];
+            while(l<=r && (products[l].length()<=i || products[l][i]!=c))
+            {
+                l++;
+            }
+            while(l<=r && (products[r].length()<=i || products[r][i]!=c))
+            {
+                r--;
+            }
+            int remain = r-l+1;
+            vector<string>temp;
+            for(int j=0;j<min(3,remain);j++)
+            {
+                temp.push_back(products[l+j]);
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
