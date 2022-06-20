eeed981e3a90cf36ab18d6f5abd7b45090836d7c
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-20 14:20:56 +0530

    Time: 34 ms (96.07%), Space: 7.1 MB (61.84%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,30 @@
+class Foo {
+public:
+    mutex a, b;
+    Foo() {
+        a.lock();
+        b.lock();
+    }
+
+    void first(function<void()> printFirst) {
+        
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        a.unlock();
+    }
+
+    void second(function<void()> printSecond) {
+        
+        // printSecond() outputs "second". Do not change or remove this line.
+        a.lock();
+        printSecond();
+        b.unlock();
+    }
+
+    void third(function<void()> printThird) {
+        
+        // printThird() outputs "third". Do not change or remove this line.
+        b.lock();
+        printThird();
+    }
+};
