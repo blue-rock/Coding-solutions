71ecf6de68169cceee917f000dbfc9ddfea395c3
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-21 19:36:35 +0530

    Time: 838 ms (8.99%), Space: 41.6 MB (82.80%) - LeetHub

---
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        vector<string>res;
+        if(words.size()==0)
+        {
+            return res;
+        }
+        set<string>s;
+        for(auto x:words)
+        {
+            s.insert(x);
+        }
+        for(auto word:words)
+        {
+            if(isconcat(word,s))
+            {
+                res.push_back(word);
+            }
+        }
+        return res;
+    }
+    bool isconcat(string word,set<string>& s)
+    {
+        int len = word.length();
+        for(int i=1;i<len;i++)
+        {
+            string suff = word.substr(i);   
+            if(s.find(word.substr(0,i))!=s.end() && (s.find(suff)!=s.end() || isconcat(suff,s)))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
