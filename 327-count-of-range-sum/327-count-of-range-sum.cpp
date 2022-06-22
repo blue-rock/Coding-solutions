a0e6572539c468b1faa7106fe3de0ce683cf4df8
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-22 15:27:07 +0530

    Time: 931 ms (47.68%), Space: 195 MB (31.94%) - LeetHub

---
+++ 327-count-of-range-sum/327-count-of-range-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int l,u;
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        vector<long long int>x(n+1,0);
+        int res = 0;
+        l = lower;
+        u = upper;
+        for(int i=0;i<n;i++)
+        {
+            x[i+1] = x[i] + nums[i];
+        }
+        merge(x.begin(),x.end(),res);
+        return res;
+    }
+    void merge(vector<long long int>::iterator start,vector<int long long>::iterator end, int&res)
+    {
+        if(end - start<=1)
+        {
+            return;
+        }
+        auto mid = start + (end-start)/2;
+        merge(start,mid,res);
+        merge(mid,end,res);
+        auto j = mid, k=mid;
+        for(auto i=start;i!=mid;i++)
+        {
+            while(j!=end &&*j - *i < l)
+            {
+                j++;
+            }
+            while(k!=end && *k - *i <=u)
+            {
+                k++;
+            }
+            res = res + k - j;
+        }
+        inplace_merge(start , mid, end);
+    }
+};
