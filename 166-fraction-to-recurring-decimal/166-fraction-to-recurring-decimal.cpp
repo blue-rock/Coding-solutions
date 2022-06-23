a64075cbbf03bd2d51488e0c5e3b6a2325424259
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-23 15:35:24 +0530

    Time: 7 ms (7.92%), Space: 7.4 MB (5.33%) - LeetHub

---
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string fractionToDecimal(int nume, int deno) {
+        if(nume==0)
+        {
+            return "0";
+        }
+        string ans="";
+        if(nume>0 ^ deno>0)
+        {
+            ans = ans + '-';
+        }
+        long num = abs(nume);
+        long den = abs(deno);
+        long r = long(num)%long(den);
+        ans = ans + to_string(num/den);
+        if(!r)
+        {
+            return ans;
+        }
+        unordered_map<int,int>mp;
+        ans = ans + ".";
+        while(r)
+        {
+            if(mp.find(r)!=mp.end())
+            {
+                ans.insert(mp[r], "(");
+                ans = ans + ")";
+                break;
+            }
+            mp[r] = ans.length();
+            r = r*10;
+            ans = ans + to_string(r/den);
+            r = r%den;
+            
+        }
+        return ans;
+    }
+};
