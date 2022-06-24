a54da05154f2311add4ae40c8d308fda36f55063
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-24 19:12:11 +0530

    Time: 97 ms (11.61%), Space: 50.7 MB (7.59%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string convert(string s, int r) {
+        if (r == 1)
+        {
+            return s;
+        }
+        int rsize = min(r,int(s.length()));
+        vector<string>v(rsize,"");
+        int dir = -1, idx = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            v[idx] += s[i];
+            idx += (dir == -1) ? 1 : -1;
+            if (idx == 0 || idx == r - 1) 
+            {
+                dir = -dir;
+            }
+        }
+        
+        string res = "";
+        for (auto a : v)
+        {
+            res = res + a ;                     
+        }
+        
+        return res;
+    }
+};
