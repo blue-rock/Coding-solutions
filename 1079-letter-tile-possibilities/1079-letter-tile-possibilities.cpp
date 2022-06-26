5d4ad15b799e0bc41a98356a2f37c9527f2f98fe
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-26 15:20:18 +0530

    Time: 223 ms (26.94%), Space: 15.2 MB (56.97%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    set<string>s;
+    map<int,int>mp;
+    string t;
+    int numTilePossibilities(string tiles) {
+        helper(tiles);
+        return s.size();
+    }
+    void helper(string& tiles)
+    {
+        for(int i=0;i<tiles.size();i++)
+        {
+            if(mp[i]!=1)
+            {
+                t.push_back(tiles[i]);
+                mp[i] = 1;
+                s.insert(t);
+                helper(tiles);
+                mp[i] = 0;
+                t.pop_back();
+            }
+        }
+    }
+};
