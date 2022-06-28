f50d1a9b1ac149f229202461f4896a7f645dcbc8
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-28 15:18:33 +0530

    Time: 86 ms (56.40%), Space: 17.2 MB (95.28%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int>freq(26,0);
+        int cantake= 0;
+        int curr = INT_MAX;
+        for(int i=0;i<s.length();i++)
+        {
+            freq[s[i]-'a']++;
+        }
+        sort(freq.begin(),freq.end());
+        reverse(freq.begin(),freq.end());
+        for(auto x:freq)
+        {
+            if(x==0)
+            {
+                break;
+            }
+            if(x<curr)
+            {
+                curr = x;
+                cantake = cantake + x;
+            }
+            else
+            {
+                curr = max(curr-1,0);
+                cantake = cantake +curr;
+            }
+        }
+        return s.length() - cantake;
+    }
+};
