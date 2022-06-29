fdc4e43f2e99902e3d0c1274cfb007fe0c157988
Heer <69032140+blue-rock@users.noreply.github.com>
2022-06-29 19:29:00 +0530

    Time: 582 ms (49.73%), Space: 84.7 MB (41.16%) - LeetHub

---
+++ 952-largest-component-size-by-common-factor/952-largest-component-size-by-common-factor.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int>parent;
+    int findpar(int x)
+    {
+        if(parent[x]==-1)
+        {
+            return x;
+        }
+        return parent[x] = findpar(parent[x]);
+    }
+    void makeunion(int x,int y)
+    {
+        int p1 = findpar(x);
+        int p2 = findpar(y);
+        if(p1!=p2)
+        {
+            parent[p1] = p2;
+        }
+    }
+    int largestComponentSize(vector<int>& nums) {
+        parent.resize(100001,-1);
+        for(auto n:nums)
+        {
+            for(int i=2;i<=sqrt(n);i++)
+            {
+                if(n%i==0)
+                {
+                    makeunion(n,i);
+                    makeunion(n,n/i);
+                }
+            }
+                
+        }
+        int count = 0;
+        unordered_map<int,int>mp;
+        for(int x:nums)
+        {
+            int p = findpar(x);
+            count = max(count,1 + mp[p]);
+            mp[p]++;
+        }
+        return count;
+    }
+};
