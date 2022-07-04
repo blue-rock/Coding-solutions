2eb005867155de67f4aa03c5fe0934902cd734a2
Heer <69032140+blue-rock@users.noreply.github.com>
2022-07-04 12:19:31 +0530

    Time: 25 ms (80.88%), Space: 18.3 MB (23.59%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int>left(n,1);
+        vector<int>right(n,1);
+        int totcand = 0;
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                left[i]= left[i-1] + 1;
+            }
+        }
+        totcand = left[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                right[i] = right[i+1] + 1;
+            }
+            totcand = totcand + max(right[i],left[i]);
+        }
+        return totcand;
+    }
+};
