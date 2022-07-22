2f9d1fe5b15cd5e6a38113b555362454c106ed12
Heer <69032140+blue-rock@users.noreply.github.com>
2022-07-22 19:52:57 +0530

    Time: 271 ms (12.41%), Space: 67.9 MB (57.76%) - LeetHub

---
+++ 848-shifting-letters/848-shifting-letters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = shifts.size();
+        for(int i=0;i<n-1;i++)
+        {
+            shifts[n-i-2] = (shifts[n-i-2]%26 +  shifts[n-i-1]%26)%26;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            s[i] = 'a' + ((s[i] - 'a') + shifts[i])%26;
+        }
+        return s;
+    }
+};
