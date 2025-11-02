ae67e6528aeedd4298be748a15484032eb1e5527
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:29:56 +0530

    Create 855-CountUniqueCharactersOfAllSubstringsOfAGivenString

---
+++ 855-CountUniqueCharactersOfAllSubstringsOfAGivenString/855-CountUniqueCharactersOfAllSubstringsOfAGivenString.cpp
@@ -0,0 +1,22 @@
+// Last updated: 11/2/2025, 10:29:56 PM
+class Solution {
+public:
+    int uniqueLetterString(string s) {
+        vector<vector<int>> idxx(26, vector<int>(1, -1)); 
+        for (int i = 0; i < s.size(); ++i)
+        {
+            idxx[s[i] - 'A'].push_back(i); 
+        }
+        
+        int ans = 0; 
+        for (int i = 0; i < 26; ++i) 
+        {
+            idxx[i].push_back(s.size()); 
+            for (int k = 1; k < idxx[i].size()-1; ++k) 
+            {
+               ans += (idxx[i][k] - idxx[i][k-1]) * (idxx[i][k+1] - idxx[i][k]);  
+            }   
+        }
+        return ans; 
+    }
+};
