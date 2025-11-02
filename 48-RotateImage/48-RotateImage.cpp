46cf433cfc5a34c5467c4feba49c5a318420323d
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:35:11 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.cpp
@@ -0,0 +1,15 @@
+// Last updated: 11/2/2025, 10:35:11 PM
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
