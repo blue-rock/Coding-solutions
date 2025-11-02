250cd1cb16feba73cb836ab8644b5b1e5eba06c0
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:33:08 +0530

    Create 203-RemoveLinkedListElements

---
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.cpp
@@ -0,0 +1,45 @@
+// Last updated: 11/2/2025, 10:33:08 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while(head!=NULL && head->val==val)
+        {
+            head = head->next;
+        }
+        if(head==NULL)
+        {
+            return head;
+        }
+        ListNode* temp = head;
+        while(temp->next!=NULL)
+        {
+            if(temp->next->val==val)
+            {
+                if(temp->next->next!=NULL)
+                {
+                    temp->next = temp->next->next;
+                }
+                else
+                {
+                    temp->next = NULL;
+                    return head;
+                }
+            }
+            else
+            {
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
