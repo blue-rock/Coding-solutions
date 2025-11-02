4bfab89072213eaf9dbf7247453b122e5b34ff88
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:31:47 +0530

    Create 435-NonOverlappingIntervals

---
+++ 435-NonOverlappingIntervals/435-NonOverlappingIntervals.cpp
@@ -0,0 +1,24 @@
+// Last updated: 11/2/2025, 10:31:47 PM
+bool comp(vector<int> &a,vector<int> &b) {
+	return a[1]<b[1];
+}
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),comp);
+        int res = -1;
+        vector<int>prev= intervals[0];
+        for(vector<int>temp:intervals)
+        {
+            if(prev[1]>temp[0])
+            {
+                res++;
+            }
+            else
+            {
+                prev = temp;
+            }
+        }
+        return res;
+    }
+};
