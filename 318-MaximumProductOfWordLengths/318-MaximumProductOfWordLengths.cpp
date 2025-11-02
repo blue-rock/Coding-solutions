6863e58e6c979cfce47be15398121b10b34cb3f6
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:32:24 +0530

    Create 318-MaximumProductOfWordLengths

---
+++ 318-MaximumProductOfWordLengths/318-MaximumProductOfWordLengths.cpp
@@ -0,0 +1,40 @@
+// Last updated: 11/2/2025, 10:32:24 PM
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int n = words.size();
+        int max_length = 0;
+        for(int i=0;i<n;i++)
+        {
+            // i is a word
+            string f_word = words[i];
+            for(int j=i+1;j<n;j++)
+            {
+                string s_word = words[j];
+                int flength = f_word.length();
+                int slength = s_word.length();
+                int flag = 0;
+                if((flength*slength)>max_length)
+                {
+                    for(int f=0;f<flength;f++)
+                    {
+                        for(int s=0;s<slength;s++)
+                        {
+                            if(f_word[f]==s_word[s])
+                            {
+                                f=flength+3;
+                                s=slength+3;
+                                flag=1;
+                            }
+                        }
+                    }
+                    if(flag==0)
+                    {
+                        max_length = flength*slength;
+                    }
+                }
+            }
+        }
+        return max_length;
+    }
+};
