9f8033a83ba668c3725f25e60b6ba88ca1ba528b
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:31:14 +0530

    Create 540-SingleElementInASortedArray

---
+++ 540-SingleElementInASortedArray/540-SingleElementInASortedArray.cpp
@@ -0,0 +1,36 @@
+// Last updated: 11/2/2025, 10:31:14 PM
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-2;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(mid%2==0)
+            {
+                if(nums[mid]!=nums[mid+1])
+                {
+                    high = mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+                if(nums[mid]==nums[mid+1])
+                {
+                    high = mid-1;
+                    
+                }
+                else
+                {
+                    low = mid+1;
+                }
+            }
+        }
+        return nums[low];
+    }
+};
