fb28680b9449316235494af5d202d2f615a87d2f
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:29:39 +0530

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.cpp
@@ -0,0 +1,19 @@
+// Last updated: 11/2/2025, 10:29:39 PM
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<nums.size();i++)
+        {
+            pq.push(nums[i]);       
+        }
+        int i=0;
+        while(!pq.empty())
+        {
+            nums[i] = pq.top();
+            pq.pop();
+            i++;
+        }
+        return nums;
+    }
+};
