cc62b6f175516451960a0097962cca22ea804c79
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:27:48 +0530

    Create 1661-MinimumNumberOfVerticesToReachAllNodes

---
+++ 1661-MinimumNumberOfVerticesToReachAllNodes/1661-MinimumNumberOfVerticesToReachAllNodes.cpp
@@ -0,0 +1,21 @@
+// Last updated: 11/2/2025, 10:27:48 PM
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int>ans;
+        vector<int>indegree(n,0);
+        for(auto i:edges)
+        {
+            indegree[i[1]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+            
+    }
+};
