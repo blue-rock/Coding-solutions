ad1152affc8bb2f5183208321d6720d5808756ff
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:28:09 +0530

    Create 1544-CountGoodNodesInBinaryTree

---
+++ 1544-CountGoodNodesInBinaryTree/1544-CountGoodNodesInBinaryTree.cpp
@@ -0,0 +1,34 @@
+// Last updated: 11/2/2025, 10:28:09 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans;
+    int goodNodes(TreeNode* root) {
+        preordertraversal(root,INT_MIN);
+        return ans;
+    }
+    void preordertraversal(TreeNode* root,int maxv)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        if(root->val>=maxv)
+        {
+            ans++;
+            maxv =root->val;
+        }
+        preordertraversal(root->left,maxv);
+        preordertraversal(root->right,maxv);
+    }
+};
