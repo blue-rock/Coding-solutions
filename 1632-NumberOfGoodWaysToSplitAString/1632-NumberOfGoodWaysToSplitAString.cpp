148556ad68057e6ab96b6c328fe6229750537a3a
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:27:54 +0530

    Create 1632-NumberOfGoodWaysToSplitAString

---
+++ 1632-NumberOfGoodWaysToSplitAString/1632-NumberOfGoodWaysToSplitAString.cpp
@@ -0,0 +1,47 @@
+// Last updated: 11/2/2025, 10:27:54 PM
+class Solution {
+public:
+    int numSplits(string s) {
+        set<char>s1;
+        set<char>s2;
+        int n = s.length();
+        if(n<=1)
+        {
+            return 0;
+        }
+        unordered_map<char,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]]++;
+        }
+        s1.insert(s[0]);
+        mp[s[0]]--;
+        for(int i=1;i<n;i++)
+        {
+            s2.insert(s[i]);
+        }
+        int ans = 0;
+        if(s1.size()==s2.size())
+        {
+            ans = 1;
+        }
+        for(int i=1;i<s.length();i++)
+        {
+               if(mp[s[i]]==1)
+               {
+                   s2.erase(s[i]);
+                   s1.insert(s[i]);
+               }
+            else
+            {
+                mp[s[i]]--;
+                s1.insert(s[i]);
+            }
+            if(s1.size()==s2.size())
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
