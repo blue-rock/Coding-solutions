55922ff3e59c4b4381096350be761ec1a4545465
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:31:25 +0530

    Create 486-PredictTheWinner

---
+++ 486-PredictTheWinner/486-PredictTheWinner.cpp
@@ -0,0 +1,17 @@
+// Last updated: 11/2/2025, 10:31:25 PM
+class Solution {
+public:
+    int check(vector<int>& nums,int s,int e)
+    {
+        if(s==e)
+        {
+            return nums[e];
+        }
+        int sumofa = nums[s] - check(nums,s+1,e);
+        int sumofb = nums[e] - check(nums,s,e-1);
+        return max(sumofa,sumofb);
+    }
+    bool PredictTheWinner(vector<int>& nums) {
+        return check(nums,0,nums.size()-1)>=0;
+    }
+};
