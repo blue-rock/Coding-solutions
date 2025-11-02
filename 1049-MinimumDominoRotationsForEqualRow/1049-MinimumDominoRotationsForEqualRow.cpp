63aea035c04abaf50f7b7d3bec5ac97397ed327e
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:29:11 +0530

    Create 1049-MinimumDominoRotationsForEqualRow

---
+++ 1049-MinimumDominoRotationsForEqualRow/1049-MinimumDominoRotationsForEqualRow.cpp
@@ -0,0 +1,31 @@
+// Last updated: 11/2/2025, 10:29:11 PM
+class Solution {
+public:
+    int huha(vector<int>&a,vector<int>&b,int num)
+    {
+        int topcount = 0;
+        int botcount = 0;
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i]!=num && b[i]!=num)
+            {
+                return -1;
+            }
+            if(a[i]!=num)
+            {
+                botcount++;
+            }
+            if(b[i]!=num)
+            {
+                topcount++;
+            }
+        }
+        return min(topcount,botcount);
+    }
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+       int f1 = huha(tops,bottoms,tops[0]);
+        int f2 = huha(tops,bottoms,bottoms[0]);
+        
+        return min(f1,f2)>0?min(f1,f2):max(f1,f2);
+    }
+};
