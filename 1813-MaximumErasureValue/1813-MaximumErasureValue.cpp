136d7530c7b4fd2944eed62cf28b1a4193a8f203
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:27:37 +0530

    Create 1813-MaximumErasureValue

---
+++ 1813-MaximumErasureValue/1813-MaximumErasureValue.cpp
@@ -0,0 +1,30 @@
+// Last updated: 11/2/2025, 10:27:37 PM
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>nums) {
+        int start = 0;
+        int end = 0;
+        int n = nums.size();
+        int ans = 0;
+        int sum = 0;
+        unordered_map<int,int>mp;
+        while(start < n && end < n){
+            
+            if(mp[nums[end]] == 0){
+                mp[nums[end]]  = 1;
+                sum += nums[end];
+                if(ans < sum){
+                    ans = sum;
+                }
+                end++;
+            }else{
+                sum -= nums[start];
+                mp[nums[start]] = 0;       
+                start++;
+            }
+            
+        }
+        
+        return ans;
+    }
+};
