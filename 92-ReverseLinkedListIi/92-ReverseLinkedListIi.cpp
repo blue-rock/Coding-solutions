60597a211425b8713f730af0444387cf50d6ca5c
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:34:31 +0530

    Create 92-ReverseLinkedListIi

---
+++ 92-ReverseLinkedListIi/92-ReverseLinkedListIi.cpp
@@ -0,0 +1,49 @@
+// Last updated: 11/2/2025, 10:34:31 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if(head==NULL)
+        {
+            return NULL;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(m>1)
+        {
+            prev = curr;
+            curr = curr->next;
+            m--;
+            n--;
+        }
+        ListNode* connect = prev;
+        ListNode* tail = curr;
+        while(n>0)
+        {
+            ListNode* nextnode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr= nextnode;
+            n--;
+        }
+        if(connect!=NULL)
+        {
+            connect->next = prev;
+        }
+        else
+        {
+            head = prev;
+        }
+        tail->next = curr;
+        return head;
+    }
+};
