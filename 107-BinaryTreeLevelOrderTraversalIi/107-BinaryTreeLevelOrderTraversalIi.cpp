a5ba2dde8afd477976f3d27548c05a0f026e2135
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:34:16 +0530

    Create 107-BinaryTreeLevelOrderTraversalIi

---
+++ 107-BinaryTreeLevelOrderTraversalIi/107-BinaryTreeLevelOrderTraversalIi.cpp
@@ -0,0 +1,52 @@
+// Last updated: 11/2/2025, 10:34:16 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL)
+        {
+            return ans;
+        }
+        if(root->left==NULL && root->right==NULL)
+        {
+            ans.push_back({root->val});
+            return ans;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int s = q.size();
+            vector<int>temp;
+            for(int i=0;i<s;i++)
+            {
+                TreeNode* curr = q.front();
+                temp.push_back(curr->val);
+                q.pop();
+                if(curr->left!=NULL)
+                {
+                    q.push(curr->left);
+                }
+                if(curr->right!=NULL)
+                {
+                    q.push(curr->right);
+                }
+            }
+            // reverse(temp.begin(),temp.end());
+            ans.push_back(temp);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
