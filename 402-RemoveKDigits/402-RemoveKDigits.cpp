43b0ec4ea9055757edccb99c98407ac7cf667357
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:31:57 +0530

    Create 402-RemoveKDigits

---
+++ 402-RemoveKDigits/402-RemoveKDigits.cpp
@@ -0,0 +1,28 @@
+// Last updated: 11/2/2025, 10:31:58 PM
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string v="";
+        for(int i=0;i<num.size();i++){
+                while(v.size()!=0 && num[i]<v.back() && k>0){
+                    v.pop_back();
+                    k--;
+                }
+                v+=num[i];
+            }
+        
+        while(k--){
+        v.pop_back();
+        }
+        
+        int i=0;
+        while(i<v.size() && v[i]=='0')
+            i++;
+        
+        v=v.substr(i);
+            
+        if(v.size()==0)
+            return "0";
+        return v;
+    }
+};
