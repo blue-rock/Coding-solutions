f430fd1b4a176edabc40d3be901dc62cc6ab1f89
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:32:27 +0530

    Create 312-BurstBalloons

---
+++ 312-BurstBalloons/312-BurstBalloons.cpp
@@ -0,0 +1,34 @@
+// Last updated: 11/2/2025, 10:32:27 PM
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>numsdp(n+2);
+        int dp[n+2][n+2];
+        memset(dp,-1,sizeof(dp));
+        numsdp[0] = 1;
+        numsdp[n+1] = 1;
+        for(int i=1;i<n+1;i++)
+        {
+            numsdp[i] = nums[i-1];
+        }
+        for(int i=2;i<=n+2;i++)
+        {
+            for(int j=0;j<n+2-i+1;j++)
+            {
+                if(i==2)
+                {
+                    dp[j][j+i-1] = 0;
+                }
+                else
+                {
+                    for(int k=j+1; k<(j+i-1); k++)
+                    {
+                        dp[j][j+i-1] = max(dp[j][j+i-1], dp[j][k] + dp[k][j+i-1] + numsdp[j]*numsdp[k]*numsdp[j+i-1]);
+                    }
+                }
+            }
+        }
+        return dp[0][n+1];
+    }
+};
