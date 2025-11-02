76e2cad0066df40daf4c906b251271e9252e8147
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:27:38 +0530

    Create 1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers

---
+++ 1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers/1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers.cpp
@@ -0,0 +1,12 @@
+// Last updated: 11/2/2025, 10:27:38 PM
+class Solution {
+public:
+    int minPartitions(string n) {
+        int maxv = 0;
+        for(int i=0;i<n.length();i++)
+        {
+            maxv = max(n[i] - '0',maxv); 
+        }
+        return maxv;
+    }
+};
