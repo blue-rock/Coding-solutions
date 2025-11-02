1bf581908a378f49687f65998de09d6059c9202c
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:33:34 +0530

    Create 148-SortList

---
+++ 148-SortList/148-SortList.cpp
@@ -0,0 +1,31 @@
+// Last updated: 11/2/2025, 10:33:34 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int>v;
+        ListNode* temp = head;
+        while(temp!=NULL)
+        {
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+        temp = head;
+        sort(v.begin(),v.end());
+        for(int i=0;i<v.size();i++)
+        {
+            temp->val = v[i];
+            temp = temp->next;
+        }
+        return head;
+    }
+};
