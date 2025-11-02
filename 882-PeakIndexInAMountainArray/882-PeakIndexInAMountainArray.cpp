527f94b4a2752f9970cca6b784f762960ce2d1ed
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:29:51 +0530

    Create 882-PeakIndexInAMountainArray

---
+++ 882-PeakIndexInAMountainArray/882-PeakIndexInAMountainArray.cpp
@@ -0,0 +1,23 @@
+// Last updated: 11/2/2025, 10:29:51 PM
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+    int s=0;
+    int e=arr.size()-1;
+
+        int mid=s + (e-s)/2;
+
+        while(s<e){
+            if(arr[mid]<arr[mid+1]){
+                s=mid + 1;
+            }
+        else
+        {
+            e=mid;
+        }
+            mid=s + (e-s)/2;
+
+        }
+        return s;
+    }
+};
