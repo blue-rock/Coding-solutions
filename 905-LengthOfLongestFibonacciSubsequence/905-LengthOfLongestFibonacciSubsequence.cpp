6eb9d4ef281ca30347e111e625c25c33c3c084e1
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:29:48 +0530

    Create 905-LengthOfLongestFibonacciSubsequence

---
+++ 905-LengthOfLongestFibonacciSubsequence/905-LengthOfLongestFibonacciSubsequence.cpp
@@ -0,0 +1,35 @@
+// Last updated: 11/2/2025, 10:29:48 PM
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>>dp(n,vector<int>(n));
+        unordered_map<int,int>mp;
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]] = i;
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int findthis = arr[j] - arr[i];
+                if(mp.find(findthis)!=mp.end())
+                {
+                    int index = mp[findthis];
+                    if(index<i)
+                    {
+                    dp[i][j] = max(dp[i][j],1+dp[index][i]);
+                    ans = max(ans,dp[i][j]);
+                    }
+                }
+            }
+        }
+        if(ans==0)
+        {
+            return ans;
+        }
+        return ans+2;
+    }
+};
