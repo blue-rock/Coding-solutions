00261ddbe8a87af0bb47f0022c05e74f24923d2c
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:32:55 +0530

    Create 221-MaximalSquare

---
+++ 221-MaximalSquare/221-MaximalSquare.cpp
@@ -0,0 +1,27 @@
+// Last updated: 11/2/2025, 10:32:55 PM
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int dp[rows][cols];
+        memset(dp,0,sizeof(dp));
+        int area = 0;
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j] = matrix[i][j] - '0';
+                }
+                else if(matrix[i][j]=='1')
+                {
+                    dp[i][j] = 1 + min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                }
+                area = max(area,dp[i][j]);
+            }
+        }
+        return area*area;
+    }
+};
