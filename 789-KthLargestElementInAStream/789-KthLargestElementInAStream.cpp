d9956d4313de7e3d283ef4441bcce70e9a4e3cba
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:30:20 +0530

    Create 789-KthLargestElementInAStream

---
+++ 789-KthLargestElementInAStream/789-KthLargestElementInAStream.cpp
@@ -0,0 +1,30 @@
+// Last updated: 11/2/2025, 10:30:20 PM
+class KthLargest {
+public:
+priority_queue<int,vector<int>, greater<int>> pq;
+int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        // return pq.top();
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>k){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
