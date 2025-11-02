fc03705b62cacaf98b46d6de70f1e6e2d8778519
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:27:21 +0530

    Create 2227-SumOfSubarrayRanges

---
+++ 2227-SumOfSubarrayRanges/2227-SumOfSubarrayRanges.cpp
@@ -0,0 +1,17 @@
+// Last updated: 11/2/2025, 10:27:21 PM
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n=nums.size();
+        long long res=0;
+        for(int i=0;i<n-1;i++){
+            int maxi=nums[i], mini=nums[i];
+            for(int j=i+1;j<n;j++){
+                if(nums[j]>maxi)maxi=nums[j];
+                else if(nums[j]<mini)mini=nums[j];
+                res+=maxi-mini;
+            }
+        }
+        return res;
+    }
+};
