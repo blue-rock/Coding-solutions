3ca34775397a88c4f5494e6c2b4576f59ad30b28
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:35:23 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.cpp
@@ -0,0 +1,25 @@
+// Last updated: 11/2/2025, 10:35:24 PM
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size()-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            if(nums[mid]<target)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+};
