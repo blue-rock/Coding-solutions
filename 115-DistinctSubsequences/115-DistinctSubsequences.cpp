6704f7fc2e6556545c03d491b80a89e4307716ae
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:34:08 +0530

    Create 115-DistinctSubsequences

---
+++ 115-DistinctSubsequences/115-DistinctSubsequences.cpp
@@ -0,0 +1,28 @@
+// Last updated: 11/2/2025, 10:34:08 PM
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int sizes = s.length();
+        int sizet = t.length();
+        vector<vector<double>>dp(sizes+1,vector<double>(sizet+1,0));
+        for(int i=0;i<=sizes;i++)
+        {
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<=sizes;i++)
+        {
+            for(int j=1;j<=sizet;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[sizes][sizet];
+    }
+};
