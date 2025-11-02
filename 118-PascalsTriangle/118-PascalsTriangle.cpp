dd60970a2732e7bd588d268197deabe7e849c2c3
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:34:05 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cpp
@@ -0,0 +1,20 @@
+// Last updated: 11/2/2025, 10:34:05 PM
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>rascal;
+        for(int i=1;i<=numRows;i++)
+        {
+            vector<int>v(i,1);
+            if(i>=3)
+            {
+            for(int j=1;j<v.size()-1;j++)
+            {
+                v[j] = rascal[i-2][j-0] + rascal[i-2][j-1];
+            }
+            }
+            rascal.push_back(v);
+        }
+        return rascal;
+    }
+};
