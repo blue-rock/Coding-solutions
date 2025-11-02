5a0bc71bc67d7ce90d19df90c22ab0983102e512
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:28:55 +0530

    Create 1184-CarPooling

---
+++ 1184-CarPooling/1184-CarPooling.cpp
@@ -0,0 +1,24 @@
+// Last updated: 11/2/2025, 10:28:55 PM
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+    vector<vector<int>>pnd;
+        int n = trips.size();
+        for(auto t:trips)
+        {
+            pnd.push_back({t[1],t[0]});
+            pnd.push_back({t[2],-t[0]});
+        }
+        sort(pnd.begin(),pnd.end());
+        int pass = 0;
+        for(int i=0;i<2*n;i++)
+        {
+            pass = pass + pnd[i][1];
+            if(pass>capacity)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
