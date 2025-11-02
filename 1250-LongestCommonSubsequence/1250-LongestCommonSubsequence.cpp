f7ea2be6935bf4716bfaee0feefc5e61b196d796
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:28:49 +0530

    Create 1250-LongestCommonSubsequence

---
+++ 1250-LongestCommonSubsequence/1250-LongestCommonSubsequence.cpp
@@ -0,0 +1,22 @@
+// Last updated: 11/2/2025, 10:28:50 PM
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int l1 = text1.size();
+        int l2 = text2.size();
+        int dp[l1+1][l2+2];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[l1][l2];
+        
+    }
+};
