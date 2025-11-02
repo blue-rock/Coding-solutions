fc1f63f78659e7ddb4a56a4851dcfc0696d622b6
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:27:23 +0530

    Create 2132-Convert1dArrayInto2dArray

---
+++ 2132-Convert1dArrayInto2dArray/2132-Convert1dArrayInto2dArray.cpp
@@ -0,0 +1,22 @@
+// Last updated: 11/2/2025, 10:27:23 PM
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if(original.size()!=m*n)
+        {
+            vector<vector<int>>res;
+            return res;
+        }
+        vector<vector<int>>res(m,vector<int>(n));
+        int p= 0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                res[i][j] = original[p];
+                p++;
+            }
+        }
+        return res;
+    }
+};
