af9b5e7e8ba1b113d5a474973b9c546f88006396
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:34:45 +0530

    Create 77-Combinations

---
+++ 77-Combinations/77-Combinations.cpp
@@ -0,0 +1,24 @@
+// Last updated: 11/2/2025, 10:34:45 PM
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>res;
+        vector<int>ans;
+        lessgoo(res,ans,n,1,k);
+        return res;
+    }
+    void lessgoo(vector<vector<int>>&res,vector<int>&ans,int n,int start,int k)
+    {
+        if(k==0)
+        {
+            res.push_back(ans);
+            return;
+        }
+        for(int i=start;i<=n;i++)
+        {
+            ans.push_back(i);
+            lessgoo(res,ans,n,i+1,k-1);
+            ans.pop_back();
+        }
+    }
+};
