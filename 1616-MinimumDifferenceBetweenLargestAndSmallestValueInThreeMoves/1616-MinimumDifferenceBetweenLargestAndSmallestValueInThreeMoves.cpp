d898b3d2a94dd32fd444ae93d2f19caff632991f
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:27:56 +0530

    Create 1616-MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves

---
+++ 1616-MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves/1616-MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.cpp
@@ -0,0 +1,17 @@
+// Last updated: 11/2/2025, 10:27:56 PM
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=4)
+        {
+            return 0;
+        }
+        sort(nums.begin(),nums.end());
+        int op1 = nums[n-4] - nums[0];
+        int op2 = nums[n-3] - nums[1];
+        int op3 = nums[n-2] - nums[2];
+        int op4 = nums[n-1] - nums[3];
+        return min(op1,min(op2,min(op3,op4)));
+    }
+};
