885fb883ceccaed07feb7c05bc6f0e17e56c3832
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:33:09 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -0,0 +1,30 @@
+// Last updated: 11/2/2025, 10:33:09 PM
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n == 1) 
+        {
+            return true;
+        }
+        unordered_map <int,int> map;
+        while (n >0) {
+            int sum =0;
+            while(n>0) {
+                int x = n%10;
+                sum+= x*x;
+                n = n/10;
+            }
+            n = sum;
+            map[n]++;
+            if(map[n] > 1)
+            {
+                return false;
+            }
+            if(n == 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
