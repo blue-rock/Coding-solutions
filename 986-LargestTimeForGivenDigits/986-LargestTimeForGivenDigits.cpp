c83f47b3dc3af7bc302c46b87fb1f37ee6e24efc
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:29:30 +0530

    Create 986-LargestTimeForGivenDigits

---
+++ 986-LargestTimeForGivenDigits/986-LargestTimeForGivenDigits.cpp
@@ -0,0 +1,29 @@
+// Last updated: 11/2/2025, 10:29:30 PM
+class Solution {
+public:
+    string largestTimeFromDigits(vector<int>& A) {
+        string result;
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                for(int k=0;k<4;k++)
+                {
+                    if(i==j || j==k || k==i)
+                    {
+                        continue;
+                    }
+                    int m = 6-i-j-k;
+                    string hh = to_string(A[i]) + to_string(A[j]);
+                    string mm = to_string(A[k]) + to_string(A[m]);
+                    string time = hh + ":" + mm;
+                    if(hh<"24" && mm<"60" && time>result)
+                    {
+                        result = time;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
