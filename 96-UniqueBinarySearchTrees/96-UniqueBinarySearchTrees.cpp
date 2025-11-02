c58ee4933a9ac8dc69c0fdac02b7163af443c839
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:34:27 +0530

    Create 96-UniqueBinarySearchTrees

---
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.cpp
@@ -0,0 +1,28 @@
+// Last updated: 11/2/2025, 10:34:27 PM
+vector<int>dp(25,0);
+class Solution {
+public:
+    int numTrees(int n) {
+        dp[0]=1;
+        dp[1]=1;
+        int total = 0;
+        if(dp[n]!=0)
+        {
+            return dp[n];
+        }
+        for(int i=2;i<=n;i++)
+        {
+            total=0;
+            for(int j=1;j<i+1;j++)
+            {
+                int left = j-1;
+                int right = i-j;
+                total = total+ dp[left]*dp[right];
+            }
+            dp[i] = total;
+        }
+        return dp[n];
+    }
+};
+
+//https://www.youtube.com/watch?v=Ox0TenN3Zpg
