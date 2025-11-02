064f9f3fa9816ed23c64d2328ecd907657b70514
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:35:25 +0530

    Create 34-FindFirstAndLastPositionOfElementInSortedArray

---
+++ 34-FindFirstAndLastPositionOfElementInSortedArray/34-FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -0,0 +1,11 @@
+// Last updated: 11/2/2025, 10:35:25 PM
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        int last = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
+        if(first > last)
+            return {-1,-1};
+        return {first, last};
+    }
+};
