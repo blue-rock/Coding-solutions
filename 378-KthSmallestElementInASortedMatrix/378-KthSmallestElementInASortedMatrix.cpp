3a995a8554d4066e5b6a07a1e5fb894953570d3a
BlueRock <69032140+blue-rock@users.noreply.github.com>
2025-11-02 22:32:02 +0530

    Create 378-KthSmallestElementInASortedMatrix

---
+++ 378-KthSmallestElementInASortedMatrix/378-KthSmallestElementInASortedMatrix.cpp
@@ -0,0 +1,21 @@
+// Last updated: 11/2/2025, 10:32:02 PM
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int low = matrix[0][0], high = matrix[n-1][n-1] + 1, mid, count, tmp;
+        
+        while (low < high) {
+            mid = low + (high - low) / 2, tmp = n - 1, count = 0;
+            for (int i = 0; i < n; i++) {
+                while (tmp >= 0 && matrix[i][tmp] > mid) tmp--;
+                count += tmp + 1;
+            }
+            
+            if (count < k) low = mid + 1;
+            else high = mid;
+        }
+        
+        return low;
+    }
+};
